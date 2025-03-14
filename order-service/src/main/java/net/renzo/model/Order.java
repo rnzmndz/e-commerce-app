e40b334fd78febe1ea2c8e1436bdbfb4a0d49931
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-03-14 20:52:36 +0800

    update

---
+++ order-service/src/main/java/net/renzo/model/Order.java
@@ -0,0 +1,17 @@
+package net.renzo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order extends Auditable{
+
+}
